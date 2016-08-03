c4ecbc133aaf3e2669a0e79d90ae4bc532ed42b6
crossoverJie <731756881@qq.com>
2016-08-03 23:13:10 +0800

    初始化

---
+++ app/src/main/java/top/crossoverjie/app/db/crossWeatherOpenHelper.java
@@ -0,0 +1,52 @@
+package top.crossoverjie.app.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class crossWeatherOpenHelper extends SQLiteOpenHelper {
+
+    /**
+     *  Province表建表语句
+     */
+    public static final String CREATE_PROVINCE = "create table Province ("
+            + "id integer primary key autoincrement, "
+            + "province_name text, "
+            + "province_code text)";
+    /**
+     *  City表建表语句
+     */
+    public static final String CREATE_CITY = "create table City ("
+            + "id integer primary key autoincrement, "
+            + "city_name text, "
+            + "city_code text, "
+            + "province_id integer)";
+    /**
+     *  County表建表语句
+     */
+    public static final String CREATE_COUNTY = "create table County ("
+            + "id integer primary key autoincrement, "
+            + "county_name text, "
+            + "county_code text, "
+            + "city_id integer)";
+
+
+    public crossWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_PROVINCE) ;
+        sqLiteDatabase.execSQL(CREATE_CITY) ;
+        sqLiteDatabase.execSQL(CREATE_COUNTY) ;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ app/src/main/java/top/crossoverjie/app/model/County.java
@@ -0,0 +1,45 @@
+package top.crossoverjie.app.model;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+public class County {
+
+    private int id ;
+    private String countyName ;
+    private String countyCode ;
+    /**城市ID**/
+    private int cityId ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getCountyCode() {
+        return countyCode;
+    }
+
+    public void setCountyCode(String countyCode) {
+        this.countyCode = countyCode;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
